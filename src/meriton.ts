ef4da612cb34a4ba21b95c3f8742618710c065f9
MarkQuach12 <mark.quach1234@gmail.com>
2025-10-19 00:52:24 +1100

    Added linting

--- src/meriton.ts
+++ src/meriton.ts
@@ -1,53 +1,64 @@
 export type meritonEnjoyer = {
-    name: string;
-    bankAccountName: string;
-    bsb: string;
-    accountNumber: string;
-    refundAmount: number;
-    isCancelled: boolean;
-    isDrinking: boolean;
-}
+	name: string;
+	bankAccountName: string;
+	bsb: string;
+	accountNumber: string;
+	refundAmount: number;
+	isCancelled: boolean;
+	isDrinking: boolean;
+};
 
 export type meritonEnjoyerOutput = {
-    name: string;
-    bankAccountName: string;
-    bsb: string;
-    accountNumber: string;
-    reimbursementAmount: number;
-}
+	name: string;
+	bankAccountName: string;
+	bsb: string;
+	accountNumber: string;
+	reimbursementAmount: number;
+};
 
-export function calculateReimbursement(group: meritonEnjoyer[], costOfMeriton: number, costOfDrinking: number): meritonEnjoyerOutput[] {
-    if (group.length === 0) {
-        throw new Error('Group should not be empty');
-    } else if (costOfMeriton < 0) {
-        throw new Error('Cost of Meriton cannot be negative');
-    } else if (costOfDrinking < 0) {
-        throw new Error('Cost of Drinking cannot be negative');
-    }
-    
-    const finalReimbursementList: meritonEnjoyerOutput[] = [] 
+export function calculateReimbursement(
+	group: meritonEnjoyer[],
+	costOfMeriton: number,
+	costOfDrinking: number
+): meritonEnjoyerOutput[] {
+	if (group.length === 0) {
+		throw new Error("Group should not be empty");
+	} else if (costOfMeriton < 0) {
+		throw new Error("Cost of Meriton cannot be negative");
+	} else if (costOfDrinking < 0) {
+		throw new Error("Cost of Drinking cannot be negative");
+	}
 
-    const attendees = group.filter(g => !g.isCancelled);
-    const drinkers = attendees.filter(g => g.isDrinking);
+	const finalReimbursementList: meritonEnjoyerOutput[] = [];
 
-    const costOfMeritonPerPerson = attendees.length > 0 ? costOfMeriton / attendees.length : 0;
-    const costOfDrinkingPerPerson = drinkers.length > 0 ? costOfDrinking / drinkers.length : 0;
+	const attendees = group.filter((g) => !g.isCancelled);
+	const drinkers = attendees.filter((g) => g.isDrinking);
 
-    for (const individual of group) {
-        const baseDeposit = individual.isDrinking ? 50 : 35;
+	const costOfMeritonPerPerson =
+		attendees.length > 0 ? costOfMeriton / attendees.length : 0;
+	const costOfDrinkingPerPerson =
+		drinkers.length > 0 ? costOfDrinking / drinkers.length : 0;
 
-        const reimbursementAmount = individual.isCancelled ? baseDeposit : baseDeposit + individual.refundAmount - costOfMeritonPerPerson - (individual.isDrinking ? costOfDrinkingPerPerson : 0);
+	for (const individual of group) {
+		const baseDeposit = individual.isDrinking ? 50 : 35;
 
-        const user: meritonEnjoyerOutput = {
-            name: individual.accountNumber,
-            bankAccountName: individual.bankAccountName,
-            bsb: individual.bsb,
-            accountNumber: individual.accountNumber,
-            reimbursementAmount: reimbursementAmount,
-        }
+		const reimbursementAmount = individual.isCancelled
+			? baseDeposit
+			: baseDeposit +
+			  individual.refundAmount -
+			  costOfMeritonPerPerson -
+			  (individual.isDrinking ? costOfDrinkingPerPerson : 0);
 
-        finalReimbursementList.push(user);
-    }
+		const user: meritonEnjoyerOutput = {
+			name: individual.accountNumber,
+			bankAccountName: individual.bankAccountName,
+			bsb: individual.bsb,
+			accountNumber: individual.accountNumber,
+			reimbursementAmount: reimbursementAmount,
+		};
 
-    return finalReimbursementList;
+		finalReimbursementList.push(user);
+	}
+
+	return finalReimbursementList;
 }
